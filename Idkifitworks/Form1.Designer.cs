45ebf640be360cc3e00e36cf2e41915e6eaf4ec6
Mahanazarnia <134784008+Mahanazarnia@users.noreply.github.com>
2024-08-15 23:34:30 +0330

    Add files via upload

---
+++ Idkifitworks/Form1.Designer.cs
@@ -0,0 +1,110 @@
+﻿namespace ExcelApp
+{
+    partial class MainForm
+    {
+        private System.ComponentModel.IContainer components = null;
+        private System.Windows.Forms.TextBox txtItemCode;
+        private System.Windows.Forms.DataGridView dataGridView;
+        private System.Windows.Forms.Button btnAdd;
+        private System.Windows.Forms.Button btnSave;
+        private System.Windows.Forms.Button btnStop;
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        private void InitializeComponent()
+        {
+            this.txtItemCode = new System.Windows.Forms.TextBox();
+            this.dataGridView = new System.Windows.Forms.DataGridView();
+            this.btnAdd = new System.Windows.Forms.Button();
+            this.btnSave = new System.Windows.Forms.Button();
+            this.btnStop = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // txtItemCode
+            // 
+            this.txtItemCode.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtItemCode.Location = new System.Drawing.Point(20, 21);
+            this.txtItemCode.Margin = new System.Windows.Forms.Padding(5, 6, 5, 6);
+            this.txtItemCode.Name = "txtItemCode";
+            this.txtItemCode.Size = new System.Drawing.Size(330, 30);
+            this.txtItemCode.TabIndex = 0;
+            // 
+            // dataGridView
+            // 
+            this.dataGridView.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.dataGridView.Location = new System.Drawing.Point(20, 88);
+            this.dataGridView.Margin = new System.Windows.Forms.Padding(5, 6, 5, 6);
+            this.dataGridView.Name = "dataGridView";
+            this.dataGridView.RowHeadersWidth = 51;
+            this.dataGridView.Size = new System.Drawing.Size(1000, 507);
+            this.dataGridView.TabIndex = 1;
+            this.dataGridView.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView_CellContentClick);
+            // 
+            // btnAdd
+            // 
+            this.btnAdd.FlatStyle = System.Windows.Forms.FlatStyle.System;
+            this.btnAdd.Font = new System.Drawing.Font("IRANYekanX", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnAdd.Location = new System.Drawing.Point(366, 16);
+            this.btnAdd.Margin = new System.Windows.Forms.Padding(5, 6, 5, 6);
+            this.btnAdd.Name = "btnAdd";
+            this.btnAdd.Size = new System.Drawing.Size(125, 38);
+            this.btnAdd.TabIndex = 2;
+            this.btnAdd.Text = "افزودن";
+            this.btnAdd.UseVisualStyleBackColor = true;
+            this.btnAdd.Click += new System.EventHandler(this.btnAdd_Click);
+            // 
+            // btnSave
+            // 
+            this.btnSave.Font = new System.Drawing.Font("IRANYekanX", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnSave.Location = new System.Drawing.Point(500, 16);
+            this.btnSave.Margin = new System.Windows.Forms.Padding(5, 6, 5, 6);
+            this.btnSave.Name = "btnSave";
+            this.btnSave.Size = new System.Drawing.Size(125, 38);
+            this.btnSave.TabIndex = 3;
+            this.btnSave.Text = "ذخیره";
+            this.btnSave.UseVisualStyleBackColor = true;
+            this.btnSave.Click += new System.EventHandler(this.btnSave_Click);
+            // 
+            // btnStop
+            // 
+            this.btnStop.Font = new System.Drawing.Font("IRANYekanX", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnStop.Location = new System.Drawing.Point(634, 16);
+            this.btnStop.Margin = new System.Windows.Forms.Padding(5, 6, 5, 6);
+            this.btnStop.Name = "btnStop";
+            this.btnStop.Size = new System.Drawing.Size(125, 38);
+            this.btnStop.TabIndex = 4;
+            this.btnStop.Text = "خروج";
+            this.btnStop.UseVisualStyleBackColor = true;
+            this.btnStop.Click += new System.EventHandler(this.btnStop_Click);
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 22F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1040, 610);
+            this.Controls.Add(this.btnStop);
+            this.Controls.Add(this.btnSave);
+            this.Controls.Add(this.btnAdd);
+            this.Controls.Add(this.dataGridView);
+            this.Controls.Add(this.txtItemCode);
+            this.Font = new System.Drawing.Font("IRANYekanX", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Margin = new System.Windows.Forms.Padding(5, 6, 5, 6);
+            this.Name = "MainForm";
+            this.Text = "تبدیل موجودی سایت";
+            this.Load += new System.EventHandler(this.MainForm_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+    }
+}
---
+++ Idkifitworks/Form1.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Windows.Forms;
+using OfficeOpenXml;
+
+namespace ExcelApp
+{
+    public partial class MainForm : Form
+    {
+        private string inputFilePath = @"C:\Users\ASUS\Desktop\NewFiles\input.xlsx";
+        private string outputFilePath = @"C:\Users\ASUS\Desktop\NewFiles\output.xlsx";
+
+        [DllImport("gdi32.dll")]
+        private static extern IntPtr AddFontResourceEx(string lpszFilename, uint fl, IntPtr pdv);
+
+        private const uint FR_PRIVATE = 0x10;
+
+        public MainForm()
+        {
+            InitializeComponent();
+            LoadCustomFont();
+            // اضافه کردن ستون‌ها به DataGridView با نام‌های جدید
+            dataGridView.Columns.Add("ItemCode", "کد کالا");
+            dataGridView.Columns.Add("ItemName", "نام کالا");
+            dataGridView.Columns.Add("PurchasePrice", "قیمت خرید");
+            dataGridView.Columns.Add("LastPurchasePrice", "آخرین قیمت خرید");
+            dataGridView.Columns.Add("DefaultSalePrice", "قیمت فروش پیشفرض");
+            dataGridView.Columns.Add("Stock", "موجودی کالا");
+        }
+
+        private void LoadCustomFont()
+        {
+            string fontPath = Path.Combine(Application.StartupPath, "Fonts", "IRANYekanX-Bold.ttf");
+            AddFontResourceEx(fontPath, FR_PRIVATE, IntPtr.Zero);
+
+            Font customFont = new Font("IRANYekanX-Bold.ttf", 12);
+            this.Font = customFont; // اعمال فونت به فرم
+            // اعمال فونت به کنترل‌های دیگر
+            dataGridView.Font = customFont;
+        }
+
+        private void btnAdd_Click(object sender, EventArgs e)
+        {
+            string itemCode = txtItemCode.Text;
+
+            using (var package = new ExcelPackage(new FileInfo(inputFilePath)))
+            {
+                var worksheet = package.Workbook.Worksheets["همگام سیستم"];
+                var cell = worksheet.Cells["A:A"].FirstOrDefault(c => c.Text == itemCode);
+                if (cell == null)
+                {
+                    MessageBox.Show("کد آیتم یافت نشد.");
+                    return;
+                }
+                int row = cell.Start.Row;
+
+                if (worksheet.Cells[row, 6].GetValue<int>() < 0)
+                {
+                    MessageBox.Show($"موجودی برای آیتم {itemCode} کمتر از صفر است.");
+                    return;
+                }
+
+                var rowData = new object[6];
+                for (int col = 1; col <= 6; col++)
+                {
+                    rowData[col - 1] = worksheet.Cells[row, col].Value;
+                }
+
+                dataGridView.Rows.Add(rowData);
+            }
+        }
+
+        private void btnStop_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void btnSave_Click(object sender, EventArgs e)
+        {
+            using (var outputPackage = new ExcelPackage(new FileInfo(outputFilePath)))
+            {
+                var outputWorksheet = outputPackage.Workbook.Worksheets.FirstOrDefault() ?? outputPackage.Workbook.Worksheets.Add("Output");
+
+                foreach (DataGridViewRow row in dataGridView.Rows)
+                {
+                    if (row.IsNewRow) continue;
+
+                    string itemCode = row.Cells[0].Value.ToString();
+                    var existingCell = outputWorksheet.Cells["A:A"].FirstOrDefault(c => c.Text == itemCode);
+                    if (existingCell != null)
+                    {
+                        int existingRow = existingCell.Start.Row;
+                        for (int col = 1; col <= 6; col++)
+                        {
+                            outputWorksheet.Cells[existingRow, col].Value = row.Cells[col - 1].Value;
+                        }
+                    }
+                    else
+                    {
+                        int newRow = (outputWorksheet.Dimension?.End.Row ?? 0) + 1;
+                        for (int col = 1; col <= 6; col++)
+                        {
+                            outputWorksheet.Cells[newRow, col].Value = row.Cells[col - 1].Value;
+                        }
+                    }
+                }
+
+                outputPackage.SaveAs(new FileInfo(outputFilePath));
+            }
+
+            MessageBox.Show("اطلاعات با موفقیت ذخیره شد.");
+        }
+
+        private void MainForm_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void dataGridView_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+    }
+}
